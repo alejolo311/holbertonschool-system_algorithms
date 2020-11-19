3c58ef57f978140af92bf48d2eb2d35e594a4574
alejolo311 <alejolo311@gmail.com>
2020-11-18 19:06:07 -0500

    all the files, the header and task 0

---
+++ 0x01-graphs/0-graph_create.c
@@ -0,0 +1,19 @@
+#include "graphs.h"
+/**
+ * graph_create - create a new Graph
+ * Return: Pointer to a Graph or NULL
+ */
+graph_t *graph_create(void)
+{
+	graph_t *new_graph = NULL;
+
+	new_graph = malloc(sizeof(graph_t));
+
+	if (new_graph == NULL)
+		return (NULL);
+
+	new_graph->nb_vertices = 0;
+	new_graph->vertices = NULL;
+
+	return (new_graph);
+}
